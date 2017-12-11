1c6412f692b1bede8c496a1daa5ef944e6604766
Jesús Saucedo <jsp.saucedo@gmail.com>
2017-12-11 02:31:06 -0600

    Add files via upload

--- parser.cpp
+++ parser.cpp
@@ -48,18 +48,10 @@ void modifica(std::string &operaciones, Funcion &f) {
 
 int main() {
     //std::string operaciones = "n   -   1/2";
-    std::string operaciones = "n   -  1";
+    std::string operaciones = "-n   -  0";
     //std::string muestras = "-23, 0.5, 5/7, -4/1, 45";
     std::string muestras = "-3, 0.5, 0.75, 3/2, -1, 11, 4, 2, 10";
 
-    /*
-    std::stringstream ss(operaciones);
-    std::string token;
-
-    while (ss >> token)
-        std::cout << token << std::endl;
-    */
-    
     Funcion info(muestras, 2);
     modifica(operaciones, info);
     print(info);
